873a9de0fea49f239952963544eaf325376980d1
JL Bautista <luis.bautista@unipolidgo.edu.mx>
2026-03-02 12:44:25 -0600

    Add Playwright test for search

---
+++ tests/search.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('find locators', async ({ page }) => {
+  test.slow();
+  await page.goto('https://playwright.dev/');
+
+  // 1. Open search modal
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  // 2. Type in the modal searchbox
+  const searchInput = page.getByRole('searchbox', { name: 'Search' });
+  await expect(searchInput).toBeVisible();
+  await searchInput.fill('locators');
+
+  // 3. Open exact Locators result from modal
+  // const locatorsLink = page.locator('a[href="/docs/locators"]');
+  const locatorsLink = page.getByRole('link', { name: 'Locators', exact: true });
+
+  // await expect(locatorsLink.first()).toBeVisible();
+  await expect(locatorsLink).toBeVisible();
+  // await locatorsLink.first().click();
+  await locatorsLink.click();
+
+  // 4. Assertions for destination page
+  await expect(page).toHaveURL(/.*locators/);
+  // await expect(page.getByRole('heading', { name: 'Locators', exact: true })).toBeVisible();
+  await expect(page.locator('h1', { hasText: 'Locators' })).toBeVisible();
+});
